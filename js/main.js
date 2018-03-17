1f0ef04883679f65c158ed5308f6ef3e62d12bba
lnoellaho <lnoellaho@gmail.com>
2018-03-17 16:34:06 -0700

    one more day

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "March 18, 2018 00:00:01",
+	date: "March 19, 2018 00:00:01",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
